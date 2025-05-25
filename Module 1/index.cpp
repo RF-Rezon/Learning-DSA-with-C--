ca709c8b50836caa835d31024e5a869969057f43
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-05-25 17:55:13 +0600

    print in C++

---
+++ Module 1/index.cpp
@@ -0,0 +1,14 @@
+// #include <stdio.h>;
+#include <iostream>;
+
+int main()
+{
+    int a = 100;
+    double b = 2.45;
+    char c = 'abcd';
+
+    // printf("%i %lf %c", a, b, c);
+    std::cout << a << " " << b << " " << c << std::endl
+              << a;
+    return 0;
+}
