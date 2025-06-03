4cfa11b8298bbb3b01b983466c60234de8b514c7
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-06-03 22:21:05 +0600

    Constructor (For DSA perpose)

--- Module 2/index.cpp
+++ Module 2/index.cpp
@@ -7,14 +7,19 @@ public:
     char name[100];
     int roll;
     double gpa;
+
+    Student(char n[], int r, double g)
+    {
+        strcpy(name, n);
+        roll = r;
+        gpa = g;
+    }
 };
 
 int main()
 {
-    Student a;
-    cin.getline(a.name, 100);
-    cin.ignore();
-    cin >> a.roll >> a.gpa;
+    Student a("Mr Rahim", 10, 4.8);
+
     cout << a.name << " " << a.roll << " " << a.gpa;
     return 0;
 }
