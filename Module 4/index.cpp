5fdf30d7d3fc7ce63e9e954ea4480f4b8476779b
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-06-10 17:35:43 +0600

    Range based for loop

---
+++ Module 4/index.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    getline(cin, name); // To take any space.
+    for (char c : name)
+    {
+        cout << c << endl;
+    }
+}
