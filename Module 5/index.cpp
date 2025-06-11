8450adf613820c95db8c28cb92bf5d69158cb909
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-06-11 18:19:49 +0600

    special comparison functor. (Not function!)

--- Module 5/index.cpp
+++ Module 5/index.cpp
@@ -9,6 +9,11 @@ public:
     float price;
 };
 
+bool cmf(Cars l, Cars r) // 😂 Kisui dilam na taw reveive korse
+{
+    return (l.price > r.price) ? true : false;
+}
+
 int main()
 {
     int n;
@@ -21,6 +26,7 @@ int main()
         getline(cin, a[i].name);
         cin >> a[i].color >> a[i].price;
     }
+    sort(a, a + n, cmf); // 😂 cmf function e kichu kintu pathai nai!
     for (int i = 0; i < n; i++)
     {
         cout << a[i].name << " " << a[i].color << " " << a[i].price << endl;
