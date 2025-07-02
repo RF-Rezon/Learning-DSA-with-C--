359ef530d33b18cf2f80700366a56c31b5f7e357
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-07-03 03:28:24 +0600

    Dynamic Node

--- Learning dsa/Introduction to dsa/index.cpp
+++ Learning dsa/Introduction to dsa/index.cpp
@@ -5,22 +5,36 @@ class Node
 {
 public:
     int val;
-    Node *index;
+    Node *next;
 
     Node(int val)
     {
         this->val = val;
+        this->next = NULL;
     }
 };
+
 int main()
 {
-    Node a(10), b(20), c(30);
 
-    a.index = &b;
-    b.index = &c;
-    c.index = NULL;
+    Node *head = new Node(10);
+    Node *body = new Node(20);
+    Node *tail = new Node(30);
 
-    cout << a.val << endl;
-    cout << a.index->val << endl;
-    cout << a.index->index->val;
+    // (*head).next = body;
+    head->next = body;
+    // (*body).next= tail;
+    body->next = tail;
+
+    // Loving Procedure
+    cout << (*head).val << endl;
+    cout << (*((*head).next)).val << endl;
+    cout << (*((*((*head).next)).next)).val << endl;
+
+    // With Arrow
+    cout << head->val << endl;
+    cout << head->next->val << endl;
+    cout << head->next->next->val << endl;
+
+    return 0;
 }
