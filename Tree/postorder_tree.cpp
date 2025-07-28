d168b772f4e452b71133986054d829608f3d57b9
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-07-28 20:49:25 +0600

    inorder, postorder tree

---
+++ Tree/postorder_tree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *left;
+    int val;
+    Node *right;
+
+    Node(int val)
+    {
+        this->left = NULL;
+        this->val = val;
+        this->right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    preorder(root->left);
+    preorder(root->right);
+    cout << root->val << " ";
+}
+
+int main()
+{
+    Node *root = new Node(50);
+    Node *a = new Node(30);
+    Node *b = new Node(20);
+    Node *c = new Node(20);
+    Node *d = new Node(10);
+    Node *e = new Node(10);
+    Node *f = new Node(10);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->left = e;
+    b->right = f;
+
+    preorder(root);
+
+    return 0;
+}
