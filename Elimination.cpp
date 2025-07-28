ac9f9c10f7b0a34743005690a51d6f1ab448b4f0
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-07-28 13:52:03 +0600

    exm questions solve stack and queue.

---
+++ Elimination.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myStack
+{
+public:
+    bool get_valid(string s)
+    {
+        stack<char> st;
+
+        for (char c : s)
+        {
+            if (c == '0')
+            {
+                st.push('0');
+            }
+            else if (c == '1')
+            {
+                if (!st.empty() && st.top() == '0')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push('1');
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        myStack obj;
+        bool x = obj.get_valid(s);
+
+        cout << (x ? "YES" : "NO") << endl;
+    }
+
+    return 0;
+}
